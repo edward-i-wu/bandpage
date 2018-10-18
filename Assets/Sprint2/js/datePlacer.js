2580a46ccdf03ae9489e835d78bd142e5ffc5d85
Edward Wu <edward.i.wu@gmail.com>
2018-10-18 14:31:56 -0400

    comments refresh and added, but NO DATE

--- Assets/Sprint2/js/datePlacer.js
+++ Assets/Sprint2/js/datePlacer.js
@@ -21,6 +21,9 @@ Array.prototype.forEach.call(comments, function(current){
     comArray.push(new comment(name, body,date));
 });
 
+//!!!reverse
+comArray=comArray.reverse();
+
 //find the form element, add event listener and stop default 
 
 var form= document.getElementById("commentform");
@@ -34,16 +37,71 @@ form.addEventListener(  'submit',
 
 function submitEvent(event){
     
-    //get info from form.elements
-    var tester = new comment("test","tester", "right now");
-    //
-    //get comments
-    clearComments(existingComments);
+    //loop through info from form.elements
+    var name;
+    var body;
+    for(var i =0; i<form.elements.length; i++){
+        //continues if not textarea input
+        if(form.elements[i].tagName.toLowerCase()!=="textarea"){
+            continue;
+        }
+
+        var value = form.elements[i].value; 
+
+        if(form.elements[i].className === "commentSection__form--nameInput"){
+            name = value; 
+        }
+        if(form.elements[i].className === "commentSection__form--commentsInput"){
+            body = value; 
+        }
+       
+    }
+    var newComment = new comment(name,body, new Date());
+    //add to your array 
+    comArray.push(newComment);
+    //clear
+    clearComments();
+    //re-render
+    renderComments();
 }
 
 //clear list passed by submitEvent
-function clearComments(list){
+function clearComments(){
+    for(var i=commentsUL.childNodes.length-1; i>=0; i--){
+         commentsUL.removeChild(commentsUL.childNodes[i]);
+        } 
+}
 
+function renderComments(){
+    var newListItem;
+    var newNameChild;
+    var newBodyChild;
+    for(var i=comArray.length-1; i>=0;i--){
+        //variable for current comment
+        var item = comArray[i];
+        //create the three nodes to be added
+        newListItem= document.createElement('li');
+        newListItem.setAttribute("class","comment");
+
+        newNameChild = document.createElement('div');
+        newNameChild.setAttribute("class","comment__name");
+
+        newBodyChild = document.createElement('div');
+        newBodyChild.setAttribute("class", "comment__body");
+        //creates text nodes
+        var textName = document.createTextNode(item.name);
+        var textBody = document.createTextNode(item.body);
+
+        //append text to divs, and divs to li
+        newNameChild.appendChild(textName);
+        newBodyChild.appendChild(textBody);
+        newListItem.appendChild(newNameChild);
+        newListItem.appendChild(newBodyChild);
+        //!!!date
+        
+        commentsUL.appendChild(newListItem);
+
+    }
 }
 
 //create new comment from form 
